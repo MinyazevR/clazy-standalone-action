25c2ed4bdd8b84a6fc3de00e56140c63f6f4cd1f
MinyazevR <eeuriset8@gmail.com>
2025-08-20 22:56:26 +0300

    Check all commits in PR

--- clazy.sh
+++ clazy.sh
@@ -38,8 +38,7 @@ fi
 pattern='^(.*?):([0-9]+):([0-9]+): (.+): (.+) \[(.*)\]$'
 
 if [[ -n "$ONLY_DIFF" ]]; then
-    TARGET_BRANCH="${GITHUB_BASE_REF:-master}"
-    for file in $(git diff --name-only origin/$TARGET_BRANCH...HEAD); do
+    for file in $(git diff --name-only -r HEAD^1 HEAD ); do
         file_extension="${file##*.}"
         if echo "$EXTENSIONS" | grep -q "$file_extension"; then
             files+=("$(realpath "$file")")
