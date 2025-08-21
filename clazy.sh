341f515d43170dc9ffb4a82b1a1f885ab9511248
MinyazevR <eeuriset8@gmail.com>
2025-08-22 02:01:36 +0300

    Use correct way to check what file already checked

--- clazy.sh
+++ clazy.sh
@@ -75,8 +75,9 @@ echo "$output" | grep -E "$pattern" | while IFS= read -r line; do
         warning_code="${BASH_REMATCH[6]}"
 
         warning_key="${absolute_path}:${line_number}:${column_number}:${warning_code}"
+        hash=$(echo -n "$warning_key" | md5sum | cut -d' ' -f1)
 
-        if [[ -n "${warnings_seen[$warning_key]:-}" ]]; then
+        if [[ -n "${warnings_seen[$hash]:-}" ]]; then
             continue
         fi
 
